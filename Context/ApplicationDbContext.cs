c7a43a294e36efb359d406f9392efaa5de41f47a
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2023-02-22 23:06:16 +0100

    Initial commit

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using University.MVC.Models;
+
+
+namespace University.MVC.Context
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+		public DbSet<User> Users { get; set; }
+	}
+}
+
---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace University.MVC.Models
+{
+	public class User
+	{
+		[Key]
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Surname { get; set; }
+
+		public string Nickname { get; set; }
+
+		public string Email { get; set; }
+
+		public string Password { get; set; }
+
+		public bool Role { get; set; }
+	}
+}
+
