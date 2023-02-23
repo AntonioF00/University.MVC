a55389b3f24ac02828b88116cf32770565f908c1
Antonio Florio <AFlorio45@gmail.com>
2023-02-23 10:35:23 +0100

    update

--- Models/User.cs
+++ Models/User.cs
@@ -11,13 +11,13 @@ namespace University.MVC.Models
 		public Guid Id { get; set; }
 
 		public string? Name { get; set; }
-					 ?
+					
 		public string? Surname { get; set; }
-					 ?
+					 
 		public string? Nickname { get; set; }
-					 ?
+					 
 		public string? Email { get; set; }
-					 ?
+					 
 		public string? Password { get; set; }
 	}
 }
