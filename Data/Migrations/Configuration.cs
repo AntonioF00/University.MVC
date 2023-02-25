ec91bb9b2b73351571b7af4adcc2da7e406a06f3
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2023-02-25 15:03:18 +0100

    add Seeding data EF Core

---
+++ Data/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using University.MVC.Context;
+using University.MVC.Models;
+
+namespace University.MVC.Data.Migrations
+{
+
+    internal sealed class Configuration : DbMigrationsConfiguration<University.MVC.Models.UniversityContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+        protected override void Seed(University.MVC.Models.UniversityContext context) 
+        {
+            context.Users.AddOrUpdate(x => x.Id,
+                                        new User() { Name = "Jane Austen" },
+                                        new User() { Name = "Charles Dickens" },
+                                        new User() { Name = "Miguel de Cervantes" }
+                                        );
+
+            context.Courses.AddOrUpdate(x => x.Id,
+                                        new Course() { Name = "Jane Austen" },
+                                        new Course() { Name = "Charles Dickens" },
+                                        new Course() { Name = "Miguel de Cervantes" }
+                                        );
+        }
+    }
+}
---
+++ Models/UniversityContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace University.MVC.Models
+{
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext() : base("name=UniversityContext")
+        {
+            this.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
+        }
+
+        public System.Data.Entity.DbSet<User> Users { get; set; }
+
+        public System.Data.Entity.DbSet<Course> Courses { get; set; }
+
+    }
+}
