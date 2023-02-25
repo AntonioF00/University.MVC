3a929a90ee702f40a9094b928bc036220b6c3e42
Antonio Florio <51435958+AntonioF00@users.noreply.github.com>
2023-02-25 15:23:43 +0100

    update seed

--- Data/Migrations/Configuration.cs
+++ Data/Migrations/Configuration.cs
@@ -1,31 +1,30 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
 using System.Data.Entity;
 using System.Data.Entity.Migrations;
-using System.Linq;
-using University.MVC.Context;
 using University.MVC.Models;
 
 namespace University.MVC.Data.Migrations
 {
-
-    internal sealed class Configuration : DbMigrationsConfiguration<University.MVC.Models.UniversityContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<UniversityContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
-        protected override void Seed(University.MVC.Models.UniversityContext context) 
+        protected override void Seed(UniversityContext context) 
         {
             context.Users.AddOrUpdate(x => x.Id,
-                                        new User() { Name = "Jane Austen" },
-                                        new User() { Name = "Charles Dickens" },
-                                        new User() { Name = "Miguel de Cervantes" }
+                                        new User() { Name = "Jane",    Surname = "Austen",  Email = "janeausten@gmail.com",   Nickname = "JaneAus",   Password = "provaJane",    Role = false},
+                                        new User() { Name = "Micheal", Surname = "Rossi",   Email = "michealrossi@gmail.com", Nickname = "MichRoss",  Password = "provaMicheal", Role = false },
+                                        new User() { Name = "Martin",  Surname = "Verdi",   Email = "martinverdi@gmail.com",  Nickname = "MartiVerd", Password = "provaMartin",  Role = true }
                                         );
 
+            var u = context.Users.FirstOrDefaultAsync(m => m.Email == "martinverdi@gmail.com");
+
             context.Courses.AddOrUpdate(x => x.Id,
-                                        new Course() { Name = "Jane Austen" },
-                                        new Course() { Name = "Charles Dickens" },
-                                        new Course() { Name = "Miguel de Cervantes" }
+                                        new Course() { Name = "PROGRAMMAZIONE" },
+                                        new Course() { Name = "SISTEMI OPERATIVI", },
+                                        new Course() { Name = "INGLESE", Id_user = u.Id,  }
                                         );
         }
     }
